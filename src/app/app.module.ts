4771b26b4253039789cce42284cd2281933d50af
amine-Laaroussi05 <amine.laaroussi05@gmail.com>
2022-06-14 23:16:16 +0200

    feat: Product List Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
 import { ProductCardComponent } from './pages/product-page/product-card/product-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductListComponent } from './pages/product-page/product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +22,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NotFoundComponent,
     HomePageComponent,
     ProductPageComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/product-page/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+  @Input()
+  titre!: string;
+
+  @Input()
+  titreImage!: string;
+
+  @Input()
+  image!: string;
+
+  @Input()
+  descriptifImage!: string;
+
+  @Input()
+  cartes: any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
